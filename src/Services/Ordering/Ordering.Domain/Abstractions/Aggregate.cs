1839ed33a23456159bba09580469f28062539a14
nxhdev2002 <xuanhoangv7c@gmail.com>
2024-06-09 23:09:12 +0700

    (feature) add ordering entities

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+        IReadOnlyList<IDomainEvent> IAggregate.DomainEvents => _domainEvents.AsReadOnly();
+
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        IDomainEvent[] IAggregate.ClearDomainEvents()
+        {
+            IDomainEvent[] dequeuedEvents = _domainEvents.ToArray();
+
+            _domainEvents.Clear();
+
+            return dequeuedEvents;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+namespace Ordering.Domain.Abstractions
+{
+    public interface IDomainEvent : INotification
+    {
+        Guid EventID => Guid.NewGuid();
+        public DateTime OccurredOn => DateTime.Now;
+        public string EventType => GetType().AssemblyQualifiedName!;
+    }
+}
