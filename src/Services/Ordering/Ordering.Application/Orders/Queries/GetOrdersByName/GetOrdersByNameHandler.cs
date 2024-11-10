3558f5a6ebff5bdfb1be74029fc4a7a30879eacb
hoangnx <xuanhoangv7c@gmail.com>
2024-11-10 16:49:04 +0700

    (feature) add get order by name handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+public class GetOrdersByNameHandler(IApplicationDbContext dbContext) 
+    : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+{
+    public async Task<GetOrdersByNameResult> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+    {
+        // get orders by name using dbContext
+        // return result
+
+        var orders = await dbContext.Orders
+            .Include(x => x.OrderItems)
+            .AsNoTracking()
+            .Where(x => x.OrderName.Value.Contains(query.Name))
+            .OrderBy(x => x.OrderName)
+            .ToListAsync(cancellationToken);
+
+        var orderDtos = orders.ToOrderDtoList();
+
+        return new GetOrdersByNameResult(orderDtos);
+    }
+}
