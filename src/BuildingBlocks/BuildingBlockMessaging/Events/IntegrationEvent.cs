96d9f70932fb7b67b910c24046c1e5c0b3991434
hoangnx <xuanhoangv7c@gmail.com>
2025-02-04 09:34:54 +0700

    Add building block messaging for rabbitmq

---
+++ src/BuildingBlocks/BuildingBlockMessaging/Events/IntegrationEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingBlockMessaging.Events;
+public record IntegrationEvent
+{
+    public Guid Id => Guid.NewGuid();
+    public DateTime? OccurredOn => DateTime.Now;
+    public string? EventName => GetType().AssemblyQualifiedName;
+}
---
+++ src/BuildingBlocks/BuildingBlockMessaging/MassTransit/Extension.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlockMessaging.MassTransit;
+public static class Extension
+{
+    public static IServiceCollection AddMassTransit(
+        this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null
+    )
+    {
+        services.AddMassTransit(config =>
+        {
+            config.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly != null)
+                config.AddConsumers(assembly);
+
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(configuration["MessageBroker:Username"]!);
+                    host.Password(configuration["MessageBroker:Password"]!);
+                });
+                configurator.ConfigureEndpoints(context);
+            });
+        });
+        return services;
+    }
+}
